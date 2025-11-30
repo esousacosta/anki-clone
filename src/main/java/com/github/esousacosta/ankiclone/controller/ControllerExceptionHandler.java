9efafeb1343691211c65e94eda26ce1fcc2c85e4
esousacosta <victoremanoel23@gmail.com>
2025-11-30 20:05:25 +0100

    Improved error formatting to use rfc-7807

--- src/main/java/com/github/esousacosta/ankiclone/controller/ControllerExceptionHandler.java
+++ src/main/java/com/github/esousacosta/ankiclone/controller/ControllerExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.github.esousacosta.ankiclone.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,4 +20,28 @@ public class ControllerExceptionHandler {
         return ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> invalidArgumentHandler(MethodArgumentNotValidException ex) {
+        List<ValidationError> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(ControllerExceptionHandler::toValidationError).collect(Collectors.toList());
+
+        ValidationErrors validationErrors = new ValidationErrors(errors);
+        ValidationErrorResponse response = new ValidationErrorResponse(
+                "/problems/validation_error",
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "One or more validation errors occurred.",
+                validationErrors
+        );
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    private static ValidationError toValidationError(FieldError fieldError) {
+        return new ValidationError(fieldError.getField(), fieldError.getDefaultMessage(), fieldError.getCode());
+    }
+
+
+    private record ValidationError(String field, String message, String code) { }
+    private record ValidationErrors(List<ValidationError> errors) {}
+    private record ValidationErrorResponse(String type, int status, String title, String detail, ValidationErrors validationErrors) { }
 }
