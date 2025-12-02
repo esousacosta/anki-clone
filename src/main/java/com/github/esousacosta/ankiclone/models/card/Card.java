8d2a618173de7c14e2d174a7c2159c981905c212
esousacosta <victoremanoel23@gmail.com>
2025-12-02 22:05:19 +0100

    Modify the data models to use JPA and add Lombok supp

--- src/main/java/com/github/esousacosta/ankiclone/models/card/Card.java
+++ src/main/java/com/github/esousacosta/ankiclone/models/card/Card.java
@@ -1,6 +1,33 @@
 package com.github.esousacosta.ankiclone.models.card;
 
+import com.github.esousacosta.ankiclone.models.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
-public record Card(int id, int userId, String front, String back, String category, boolean lastAttemptCorrect, LocalDateTime lastReviewed) {
+@Entity
+@Table(name = "cards")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    @Column(name = "front", nullable = false)
+    private String front;
+    @Column(name = "back", nullable = false)
+    private String back;
+    @Column(name = "category", nullable = true)
+    private String category;
+    @Column(name = "last_attempt_correct", nullable = false)
+    private boolean lastAttemptCorrect;
+    @Column(name = "last_reviewed", nullable = false)
+    private LocalDateTime lastReviewed;
 }
--- src/main/java/com/github/esousacosta/ankiclone/models/user/User.java
+++ src/main/java/com/github/esousacosta/ankiclone/models/user/User.java
@@ -1,6 +1,27 @@
 package com.github.esousacosta.ankiclone.models.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
-public record User(int id, String firstName, String lastName, String username, LocalDateTime lastLoginDate) {
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "last_login_date", nullable = true)
+    private LocalDateTime lastLoginDate;
 }
